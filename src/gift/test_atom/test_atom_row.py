8b9adcf5af32e13257b5c587c0e1796a11d214a9
Joachim <jschalk@users.noreply.github.com>
2024-09-03 21:48:02 -0700

    #298 atomrow concept draft

---
+++ src/gift/test_atom/test_atom_row.py
@@ -0,0 +1,50 @@
+# from src.gift.atom_config import bud_acctunit_text
+# from src.gift.atom import AtomRow
+
+
+# def test_AtomRow_exists():
+#     # ESTABLISH /  WHEN
+#     x_stonerow = AtomRow()
+
+#     # THEN
+#     assert x_stonerow.acct_id is None
+#     assert x_stonerow.addin is None
+#     assert x_stonerow.base is None
+#     assert x_stonerow.base_idea_active_requisite is None
+#     assert x_stonerow.begin is None
+#     assert x_stonerow.bit is None
+#     assert x_stonerow.close is None
+#     assert x_stonerow.credit_score is None
+#     assert x_stonerow.credit_vote is None
+#     assert x_stonerow.credor_respect is None
+#     assert x_stonerow.debtit_score is None
+#     assert x_stonerow.debtit_vote is None
+#     assert x_stonerow.debtor_respect is None
+#     assert x_stonerow.denom is None
+#     assert x_stonerow.divisor is None
+#     assert x_stonerow.fnigh is None
+#     assert x_stonerow.fopen is None
+#     assert x_stonerow.fund_coin is None
+#     assert x_stonerow.fund_pool is None
+#     assert x_stonerow.give_force is None
+#     assert x_stonerow.gogo_want is None
+#     assert x_stonerow.group_id is None
+#     assert x_stonerow.healer_id is None
+#     assert x_stonerow.label is None
+#     assert x_stonerow.mass is None
+#     assert x_stonerow.max_tree_traverse is None
+#     assert x_stonerow.monetary_desc is None
+#     assert x_stonerow.morph is None
+#     assert x_stonerow.need is None
+#     assert x_stonerow.nigh is None
+#     assert x_stonerow.numor is None
+#     assert x_stonerow.open is None
+#     assert x_stonerow.parent_road is None
+#     assert x_stonerow.penny is None
+#     assert x_stonerow.pick is None
+#     assert x_stonerow.pledge is None
+#     assert x_stonerow.problem_bool is None
+#     assert x_stonerow.road is None
+#     assert x_stonerow.stop_want is None
+#     assert x_stonerow.take_force is None
+#     assert x_stonerow.tally is None
