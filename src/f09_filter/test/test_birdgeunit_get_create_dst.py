b390a6d01f7235135989296d5bd1f52238a6d07a
jschalk <joachim.schalk@gmail.com>
2024-10-15 15:23:49 -0700

    cleanup

--- src/f09_filter/test/test_birdgeunit_get_create_dst.py
+++ src/f09_filter/test/test_birdgeunit_get_create_dst.py
@@ -39,17 +39,17 @@ def test_BridgeUnit_get_create_dst_ReturnsObjAndSetsAttr_road_Scenario0():
     src_music45_str = "music45"
     src_r_delimiter = "/"
     dst_r_delimiter = ":"
-    label_bridgeunit = bridgeunit_shop(road_str(), src_r_delimiter, dst_r_delimiter)
-    assert label_bridgeunit.src_exists(src_music45_str) is False
-    assert label_bridgeunit.src_to_dst_exists(src_music45_str, src_music45_str) is False
+    road_bridgeunit = bridgeunit_shop(road_str(), src_r_delimiter, dst_r_delimiter)
+    assert road_bridgeunit.src_exists(src_music45_str) is False
+    assert road_bridgeunit.src_to_dst_exists(src_music45_str, src_music45_str) is False
 
     # WHEN
-    gen_dst_road = label_bridgeunit.get_create_dst(src_music45_str)
+    gen_dst_road = road_bridgeunit.get_create_dst(src_music45_str)
 
     # THEN
     assert gen_dst_road == src_music45_str
-    assert label_bridgeunit.src_exists(src_music45_str)
-    assert label_bridgeunit.src_to_dst_exists(src_music45_str, src_music45_str)
+    assert road_bridgeunit.src_exists(src_music45_str)
+    assert road_bridgeunit.src_to_dst_exists(src_music45_str, src_music45_str)
 
 
 def test_BridgeUnit_get_create_dst_ReturnsObjAndSetsAttr_road_Scenario1():
@@ -60,30 +60,30 @@ def test_BridgeUnit_get_create_dst_ReturnsObjAndSetsAttr_road_Scenario1():
     dst_r_delimiter = ":"
     clean_src_str = "clean"
     clean_src_road = f"{src_music45_str}{src_r_delimiter}{clean_src_str}"
-    label_bridgeunit = bridgeunit_shop(road_str(), src_r_delimiter, dst_r_delimiter)
-    assert label_bridgeunit.src_exists(src_music45_str) is False
-    assert label_bridgeunit.src_exists(clean_src_road) is False
+    road_bridgeunit = bridgeunit_shop(road_str(), src_r_delimiter, dst_r_delimiter)
+    assert road_bridgeunit.src_exists(src_music45_str) is False
+    assert road_bridgeunit.src_exists(clean_src_road) is False
 
     # WHEN
-    gen_dst_road = label_bridgeunit.get_create_dst(clean_src_road)
+    gen_dst_road = road_bridgeunit.get_create_dst(clean_src_road)
 
     # THEN
     assert gen_dst_road is None
-    assert label_bridgeunit.src_exists(src_music45_str) is False
-    assert label_bridgeunit.src_exists(clean_src_road) is False
-    assert label_bridgeunit.src_to_dst_exists(src_music45_str, dst_music87_str) is False
+    assert road_bridgeunit.src_exists(src_music45_str) is False
+    assert road_bridgeunit.src_exists(clean_src_road) is False
+    assert road_bridgeunit.src_to_dst_exists(src_music45_str, dst_music87_str) is False
 
     # ESTABLISH
-    label_bridgeunit.set_src_to_dst(src_music45_str, dst_music87_str)
-    assert label_bridgeunit.src_to_dst_exists(src_music45_str, dst_music87_str)
-    assert label_bridgeunit.src_exists(clean_src_road) is False
+    road_bridgeunit.set_src_to_dst(src_music45_str, dst_music87_str)
+    assert road_bridgeunit.src_to_dst_exists(src_music45_str, dst_music87_str)
+    assert road_bridgeunit.src_exists(clean_src_road) is False
 
     # WHEN
-    gen_dst_road = label_bridgeunit.get_create_dst(clean_src_road)
+    gen_dst_road = road_bridgeunit.get_create_dst(clean_src_road)
 
     # THEN
-    assert label_bridgeunit.src_exists(clean_src_road)
-    assert label_bridgeunit.src_to_dst_exists(clean_src_road, gen_dst_road)
+    assert road_bridgeunit.src_exists(clean_src_road)
+    assert road_bridgeunit.src_to_dst_exists(clean_src_road, gen_dst_road)
     assert gen_dst_road == f"{dst_music87_str}{dst_r_delimiter}{clean_src_str}"
 
 
@@ -93,24 +93,26 @@ def test_BridgeUnit_get_create_dst_ReturnsObjAndSetsAttr_group_id():
     src_r_delimiter = "/"
     swim_src = f"swim{src_r_delimiter}"
     climb_src = f"climb{src_r_delimiter}_{dst_r_delimiter}"
-    label_bridgeunit = bridgeunit_shop(group_id_str(), src_r_delimiter, dst_r_delimiter)
-    label_bridgeunit.src_exists(swim_src) is False
-    label_bridgeunit.src_exists(climb_src) is False
+    group_id_bridgeunit = bridgeunit_shop(
+        group_id_str(), src_r_delimiter, dst_r_delimiter
+    )
+    group_id_bridgeunit.src_exists(swim_src) is False
+    group_id_bridgeunit.src_exists(climb_src) is False
 
     # WHEN
     swim_dst = f"swim{dst_r_delimiter}"
-    assert label_bridgeunit.get_create_dst(swim_src) == swim_dst
+    assert group_id_bridgeunit.get_create_dst(swim_src) == swim_dst
 
     # THEN
-    assert label_bridgeunit.src_exists(swim_src)
-    assert label_bridgeunit.src_exists(climb_src) is False
-    assert label_bridgeunit._get_dst_value(swim_src) == swim_dst
+    assert group_id_bridgeunit.src_exists(swim_src)
+    assert group_id_bridgeunit.src_exists(climb_src) is False
+    assert group_id_bridgeunit._get_dst_value(swim_src) == swim_dst
 
     # WHEN
-    assert label_bridgeunit.get_create_dst(climb_src) == None
+    assert group_id_bridgeunit.get_create_dst(climb_src) is None
     # THEN
-    assert label_bridgeunit.src_exists(swim_src)
-    assert label_bridgeunit.src_exists(climb_src) is False
+    assert group_id_bridgeunit.src_exists(swim_src)
+    assert group_id_bridgeunit.src_exists(climb_src) is False
 
 
 def test_BridgeUnit_get_create_dst_AddsMissingElementsTo_src_to_dst():
