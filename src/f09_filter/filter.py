4b5ceba16161c67774d89407d963f61b6b46b475
jschalk <joachim.schalk@gmail.com>
2024-10-16 12:02:10 -0700

    #342 filter works on multi column dataframes

--- src/f09_filter/filter.py
+++ src/f09_filter/filter.py
@@ -8,7 +8,8 @@ def filter_single_column_dataframe(
     if x_bridgeunit.atom_arg in x_dt:
         row_count = len(x_dt)
         for cur_row in range(row_count):
-            x_dt.loc[cur_row] = x_bridgeunit.get_create_dst(
-                x_dt.iloc[cur_row][x_bridgeunit.atom_arg]
-            )
+            x_column = x_bridgeunit.atom_arg
+            src_value = x_dt.iloc[cur_row][x_column]
+            dst_value = x_bridgeunit.get_create_dst(src_value)
+            x_dt.at[cur_row, x_column] = dst_value
     return x_dt
--- src/f09_filter/test/test_filter_.py
+++ src/f09_filter/test/test_filter_.py
@@ -1,3 +1,4 @@
+from src.f04_gift.atom_config import acct_id_str, fiscal_id_str, credit_belief_str
 from src.f09_filter.bridge import bridgeunit_shop
 from src.f09_filter.filter import filter_single_column_dataframe
 from pandas import DataFrame
@@ -7,7 +8,7 @@ from copy import deepcopy as copy_deepcopy
 def test_filter_single_column_dataframe_ReturnsObj_Scenario0_AcctID_EmptyDataFrame():
     # ESTABLISH
     acct_id_bridgeunit = bridgeunit_shop("acct_id")
-    empty_dt = DataFrame(columns=["acct_id"])
+    empty_dt = DataFrame(columns=[acct_id_str()])
 
     # WHEN
     gen_dt = filter_single_column_dataframe(empty_dt, acct_id_bridgeunit)
@@ -18,7 +19,6 @@ def test_filter_single_column_dataframe_ReturnsObj_Scenario0_AcctID_EmptyDataFra
 
 def test_filter_single_column_dataframe_ReturnsObj_Scenario0_AcctID_5rows():
     # ESTABLISH
-    acct_id_str = "acct_id"
     xio_src = "Xio"
     sue_src = "Sue"
     bob_src = "Bob"
@@ -26,11 +26,11 @@ def test_filter_single_column_dataframe_ReturnsObj_Scenario0_AcctID_5rows():
     xio_dst = "Xioita"
     sue_dst = "Suita"
     bob_dst = "Bobita"
-    acct_id_bridgeunit = bridgeunit_shop(acct_id_str)
+    acct_id_bridgeunit = bridgeunit_shop(acct_id_str())
     acct_id_bridgeunit.set_src_to_dst(xio_src, xio_dst)
     acct_id_bridgeunit.set_src_to_dst(sue_src, sue_dst)
     acct_id_bridgeunit.set_src_to_dst(bob_src, bob_dst)
-    src_dt = DataFrame(columns=[acct_id_str])
+    src_dt = DataFrame(columns=[acct_id_str()])
     src_dt.loc[0] = [zia_src]
     src_dt.loc[1] = [sue_src]
     src_dt.loc[2] = [bob_src]
@@ -43,14 +43,53 @@ def test_filter_single_column_dataframe_ReturnsObj_Scenario0_AcctID_5rows():
     filter_single_column_dataframe(src_dt, acct_id_bridgeunit)
 
     # THEN
-    assert src_dt.iloc[0][acct_id_str] == zia_src
-    assert src_dt.iloc[1][acct_id_str] == sue_dst
+    assert src_dt.iloc[0][acct_id_str()] == zia_src
+    assert src_dt.iloc[1][acct_id_str()] == sue_dst
     assert src_dt.to_csv() != old_src_dt.to_csv()
-    dst_dt = DataFrame(columns=[acct_id_str])
+    dst_dt = DataFrame(columns=[acct_id_str()])
     dst_dt.loc[0] = zia_src
     dst_dt.loc[1] = sue_dst
     dst_dt.loc[2] = bob_dst
     dst_dt.loc[3] = xio_dst
+    print(f"{str(src_dt.to_csv())=}")
+    print(f"{str(dst_dt.to_csv())=}")
+    assert src_dt.to_csv() == dst_dt.to_csv()
+
+
+def test_filter_single_column_dataframe_ReturnsObj_Scenario1_AcctID_5rowsMultipleColumns():
+    # ESTABLISH
+    xio_src = "Xio"
+    sue_src = "Sue"
+    bob_src = "Bob"
+    zia_src = "Zia"
+    xio_dst = "Xioita"
+    sue_dst = "Suita"
+    bob_dst = "Bobita"
+    acct_id_bridgeunit = bridgeunit_shop(acct_id_str())
+    acct_id_bridgeunit.set_src_to_dst(xio_src, xio_dst)
+    acct_id_bridgeunit.set_src_to_dst(sue_src, sue_dst)
+    acct_id_bridgeunit.set_src_to_dst(bob_src, bob_dst)
+    src_dt = DataFrame(columns=[fiscal_id_str(), acct_id_str(), credit_belief_str()])
+    src_dt.loc[0] = ["ZZ", zia_src, 12]
+    src_dt.loc[1] = ["ZZ", sue_src, 12]
+    src_dt.loc[2] = ["ZZ", bob_src, 12]
+    src_dt.loc[3] = ["ZZ", xio_src, 12]
+    src_dt = src_dt.reset_index(drop=True)
+    old_src_dt = copy_deepcopy(src_dt)
+    print(f"{src_dt=}")
+
+    # WHEN
+    filter_single_column_dataframe(src_dt, acct_id_bridgeunit)
+
+    # THEN
+    assert src_dt.iloc[0][acct_id_str()] == zia_src
+    assert src_dt.iloc[1][acct_id_str()] == sue_dst
+    assert src_dt.to_csv() != old_src_dt.to_csv()
+    dst_dt = DataFrame(columns=[fiscal_id_str(), acct_id_str(), credit_belief_str()])
+    dst_dt.loc[0] = ["ZZ", zia_src, 12]
+    dst_dt.loc[1] = ["ZZ", sue_dst, 12]
+    dst_dt.loc[2] = ["ZZ", bob_dst, 12]
+    dst_dt.loc[3] = ["ZZ", xio_dst, 12]
     print(f"{str(src_dt.to_csv())=}")
     print(f"{str(dst_dt.to_csv())=}")
     assert src_dt.to_csv() == dst_dt.to_csv()
