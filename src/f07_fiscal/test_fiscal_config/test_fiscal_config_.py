f89f912a787e57b2ab5838f597c7590be8d6b92d
jschalk <joachim.schalk@gmail.com>
2024-10-28 13:16:38 -0700

    #359 building fiscal_config.json required and optional args

--- src/f07_fiscal/test_fiscal_config/test_fiscal_config_.py
+++ src/f07_fiscal/test_fiscal_config/test_fiscal_config_.py
@@ -4,7 +4,16 @@ from src.f03_chrono.chrono import (
     yr1_jan1_offset_str,
     monthday_distortion_str,
 )
-from src.f04_gift.atom_config import fiscal_id_str, penny_str
+from src.f04_gift.atom_config import (
+    fiscal_id_str,
+    penny_str,
+    required_args_str,
+    optional_args_str,
+    atom_update,
+    atom_insert,
+    atom_delete,
+    normal_specs_str,
+)
 from src.f07_fiscal.fiscal_config import (
     config_file_dir,
     get_fiscal_config_file_name,
@@ -17,6 +26,13 @@ from src.f07_fiscal.fiscal_config import (
     cumlative_day_str,
     weekday_label_str,
     weekday_order_str,
+    fiscalunit_str,
+    fiscal_purviewlog_str,
+    fiscal_purview_episode_str,
+    fiscal_cashbook_str,
+    fiscal_timeline_hour_str,
+    fiscal_timeline_month_str,
+    fiscal_timeline_weekday_str,
 )
 from os import getcwd as os_getcwd
 
@@ -30,6 +46,13 @@ def test_str_functions_ReturnsObj():
     assert cumlative_day_str() == "cumlative_day"
     assert weekday_label_str() == "weekday_label"
     assert weekday_order_str() == "weekday_order"
+    assert fiscalunit_str() == "fiscalunit"
+    assert fiscal_purviewlog_str() == "fiscal_purviewlog"
+    assert fiscal_purview_episode_str() == "fiscal_purview_episode"
+    assert fiscal_cashbook_str() == "fiscal_cashbook"
+    assert fiscal_timeline_hour_str() == "fiscal_timeline_hour"
+    assert fiscal_timeline_month_str() == "fiscal_timeline_month"
+    assert fiscal_timeline_weekday_str() == "fiscal_timeline_weekday"
 
 
 def test_get_fiscal_config_file_name_ReturnsObj():
@@ -43,16 +66,60 @@ def test_config_file_dir_ReturnsObj() -> str:
 
 def test_get_fiscal_config_dict_ReturnsObj():
     # ESTABLISH / WHEN
-    x_fiscal_config = get_fiscal_config_dict()
+    fiscal_config = get_fiscal_config_dict()
 
     # THEN
-    assert x_fiscal_config
-    fiscal_config_categorys = set(x_fiscal_config.keys())
-    assert "fiscalunit" in fiscal_config_categorys
-    assert "fiscal_purviewlog" in fiscal_config_categorys
-    assert "fiscal_purview_episode" in fiscal_config_categorys
-    assert "fiscal_cashbook" in fiscal_config_categorys
-    assert "fiscal_timeline_hour" in fiscal_config_categorys
-    assert "fiscal_timeline_month" in fiscal_config_categorys
-    assert "fiscal_timeline_weekday" in fiscal_config_categorys
-    assert len(x_fiscal_config) == 7
+    assert fiscal_config
+    fiscal_config_categorys = set(fiscal_config.keys())
+    assert fiscalunit_str() in fiscal_config_categorys
+    assert fiscal_purviewlog_str() not in fiscal_config_categorys
+    assert fiscal_purview_episode_str() in fiscal_config_categorys
+    assert fiscal_cashbook_str() in fiscal_config_categorys
+    assert fiscal_timeline_hour_str() in fiscal_config_categorys
+    assert fiscal_timeline_month_str() in fiscal_config_categorys
+    assert fiscal_timeline_weekday_str() in fiscal_config_categorys
+    assert len(fiscal_config) == 6
+    _validate_fiscal_config(fiscal_config)
+    fiscalunit_dict = fiscal_config.get(fiscalunit_str())
+    fiscal_purview_episode_dict = fiscal_config.get(fiscal_purview_episode_str())
+    fiscal_cashbook_dict = fiscal_config.get(fiscal_cashbook_str())
+    fiscal_timeline_hour_dict = fiscal_config.get(fiscal_timeline_hour_str())
+    fiscal_timeline_month_dict = fiscal_config.get(fiscal_timeline_month_str())
+    fiscal_timeline_weekday_dict = fiscal_config.get(fiscal_timeline_weekday_str())
+    assert len(fiscalunit_dict.get(required_args_str())) == 0
+    assert len(fiscal_purview_episode_dict.get(required_args_str())) == 4
+    assert len(fiscal_cashbook_dict.get(required_args_str())) == 4
+    assert len(fiscal_timeline_hour_dict.get(required_args_str())) == 3
+    assert len(fiscal_timeline_month_dict.get(required_args_str())) == 3
+    assert len(fiscal_timeline_weekday_dict.get(required_args_str())) == 3
+    assert len(fiscalunit_dict.get(optional_args_str())) == 0
+    assert len(fiscal_purview_episode_dict.get(optional_args_str())) == 1
+    assert len(fiscal_cashbook_dict.get(optional_args_str())) == 1
+    assert len(fiscal_timeline_hour_dict.get(optional_args_str())) == 0
+    assert len(fiscal_timeline_month_dict.get(optional_args_str())) == 0
+    assert len(fiscal_timeline_weekday_dict.get(optional_args_str())) == 0
+
+
+# fiscalunit        "c400_config,current_time,fiscal_id,fund_coin,monthday_distortion,penny,respect_bit,road_delimiter,timeline_label,yr1_jan1_offset":
+# fiscal_timeline_hour        "cumlative_minute,fiscal_id,hour_label"
+# fiscal_timeline_month        "cumlative_day,fiscal_id,month_label"
+# fiscal_timeline_weekday        "fiscal_id,weekday_label,weekday_order"
+
+
+def _validate_fiscal_config(fiscal_config: dict):
+    # for every fiscal_format file there exists a unique fiscal_number always with leading zeros to make 5 digits
+    for fiscal_category, fiscal_dict in fiscal_config.items():
+        print(f"_validate_fiscal_config {fiscal_category=}")
+        assert fiscal_dict.get(required_args_str()) is not None
+        assert fiscal_dict.get(optional_args_str()) is not None
+        assert fiscal_dict.get(atom_update()) is None
+        assert fiscal_dict.get(atom_insert()) is None
+        assert fiscal_dict.get(atom_delete()) is None
+        assert fiscal_dict.get(normal_specs_str()) is None
+
+        fiscal_required_args_keys = set(fiscal_dict.get(required_args_str()).keys())
+        # print(f"{atom_required_args_keys=}")
+        # print(f"{fiscal_required_args_keys=}")
+        fiscal_optional_args_keys = set(fiscal_dict.get(optional_args_str()).keys())
+        # print(f" {atom_optional_args_keys=}")
+        # print(f"{fiscal_optional_args_keys=}")
