0d8537ee3a262d0ad088ed883a900ee4432ed3b8
jschalk <joachim.schalk@gmail.com>
2024-11-05 21:15:23 -0800

    #46 refactor table tests

--- src/f09_brick/test_brick_format/test_brick_models.py
+++ src/f09_brick/test_brick_format/test_brick_models.py
@@ -32,41 +32,48 @@ def check_sqlalchemytableclass(
     # WHEN / THEN
     assert SqlAlchemyTable.__tablename__ == f"{brick_number}_{table_type}"
     for x_column, column_type in extra_columns.items():
-        src_type_column = mapper.columns.get(x_column)
-        assert src_type_column is not None
-        assert str(src_type_column.type) == column_type
+        _check_sqlalchemycolumn(x_column, mapper, SqlAlchemyTable, column_type)
 
     brickref_columns = get_quick_bricks_column_ref().get(brick_number)
-    _check_columns(brickref_columns, mapper, SqlAlchemyTable)
+    for brickref_column in brickref_columns:
+        _check_sqlalchemycolumn(brickref_column, mapper, SqlAlchemyTable)
+
     assert len(mapper.columns) == len(brickref_columns) + len(extra_columns)
 
 
-def _check_columns(brickref_columns, mapper, SqlAlchemyTable):
-    for brickref_column in brickref_columns:
-        x_column = mapper.columns.get(brickref_column)
-        failed_assert_str = f"{brickref_column=} is missing from TableModel {SqlAlchemyTable.__tablename__}"
-        assert x_column is not None, failed_assert_str
+def _check_sqlalchemycolumn(
+    brickref_column: str, mapper, SqlAlchemyTable, column_type: str = None
+):
+    x_column = mapper.columns.get(brickref_column)
+    failed_assert_str = (
+        f"{brickref_column=} is missing from TableModel {SqlAlchemyTable.__tablename__}"
+    )
+    assert x_column is not None, failed_assert_str
+    if column_type is None:
         column_type = get_brick_sqlite_type().get(brickref_column)
-        if column_type == "TEXT":
-            column_type = "VARCHAR"
-        elif column_type == "REAL":
-            column_type = "FLOAT"
-        failed_assert_str = (
-            f"Table '{SqlAlchemyTable.__tablename__}' {x_column=} {column_type=}"
-        )
-        assert str(x_column.type) == column_type, failed_assert_str
+    if column_type == "TEXT":
+        column_type = "VARCHAR"
+    elif column_type == "REAL":
+        column_type = "FLOAT"
+    failed_assert_str = (
+        f"Table '{SqlAlchemyTable.__tablename__}' {x_column=} {column_type=}"
+    )
+    assert str(x_column.type) == column_type, failed_assert_str
 
 
 def test_HoldTable_ClassesHaveCorrectAttrs():
+    # ESTABLISH / WHEN / THEN
     for brick_number, HoldTable in get_brick_holdtables().items():
         check_sqlalchemytableclass(brick_number, HoldTable, "hold", {})
 
 
 def test_StageTable_ClassesHaveCorrectAttrs():
+    # ESTABLISH
     extra_columns = {
         "src_type": "VARCHAR",
         "src_path": "VARCHAR",
         "src_sheet": "VARCHAR",
     }
+    # WHEN / THEN
     for brick_number, StageTable in get_brick_stagetables().items():
         check_sqlalchemytableclass(brick_number, StageTable, "stage", extra_columns)
