2677e7547a75da7362ef20db7c258bb1c4c620e3
jschalk <joachim.schalk@gmail.com>
2024-12-08 13:05:11 -0800

    #381 worldunit.event_pidgins_csvs_to_pidgin_jsons method created

--- src/f10_world/world.py
+++ src/f10_world/world.py
@@ -30,6 +30,7 @@ from src.f10_world.transformers import (
     etl_pidgin_agg_to_face_dirs,
     etl_face_pidgins_to_event_pidgins,
     etl_event_pidgins_to_pidgin_csv_files,
+    etl_event_pidgins_csvs_to_pidgin_jsons,
 )
 from pandas import read_excel as pandas_read_excel
 from dataclasses import dataclass
@@ -155,6 +156,9 @@ class WorldUnit:
 
     def event_pidgins_to_pidgin_csv_files(self):
         etl_event_pidgins_to_pidgin_csv_files(self._faces_dir)
+
+    def event_pidgins_csvs_to_pidgin_jsons(self):
+        etl_event_pidgins_csvs_to_pidgin_jsons(self._faces_dir)
 
     def get_dict(self) -> dict:
         return {
