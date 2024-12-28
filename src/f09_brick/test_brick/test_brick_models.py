6768c742fce34bfc8c91f0bcd02acc92b4af82bf
jschalk <joachim.schalk@gmail.com>
2024-12-28 12:30:34 -0800

    testing bug fix

--- src/f09_brick/test_brick/test_brick_models.py
+++ src/f09_brick/test_brick/test_brick_models.py
@@ -98,7 +98,7 @@ def test_Base_create_all_CreatesTables_Scenario0_InMemory():
 
 def test_Base_create_all_CreatesTables_Scenario1_File(brick_env_setup_cleanup):
     # ESTABLISH
-    engine = create_engine(f"sqlite:///{brick_examples_dir()}mydatabase3.db")
+    engine = create_engine(f"sqlite:///{brick_examples_dir()}/mydatabase3.db")
 
     # WHEN
     Base.metadata.create_all(engine)
