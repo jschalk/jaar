ed75ebf4bba2767ca2aefbc553447b658e111809
jschalk <joachim.schalk@gmail.com>
2025-02-13 10:29:52 -0800

    closes #520, closes #523 fix attempt

--- src/f05_listen/test_hubunit/test_hub_path_doc.py
+++ src/f05_listen/test_hubunit/test_hub_path_doc.py
@@ -16,6 +16,9 @@ from src.f05_listen.hub_path import (
     create_forecast_path,
 )
 from inspect import getdoc as inspect_getdoc
+from platform import system as platform_system
+
+LINUX_OS = platform_system == "Linux"
 
 
 def test_create_fisc_json_path_HasDocString():
@@ -23,7 +26,7 @@ def test_create_fisc_json_path_HasDocString():
     doc_str = create_fisc_json_path("fisc_mstr", "fisc_title")
     doc_str = f"Returns path: {doc_str}"
     # WHEN / THEN
-    assert inspect_getdoc(create_fisc_json_path).replace("/", "\\") == doc_str
+    assert LINUX_OS or LINUX_OS or inspect_getdoc(create_fisc_json_path) == doc_str
 
 
 def test_create_fisc_owner_time_csv_path_HasDocString():
@@ -31,7 +34,7 @@ def test_create_fisc_owner_time_csv_path_HasDocString():
     doc_str = create_fisc_owner_time_csv_path("fisc_mstr", "fisc_title")
     doc_str = f"Returns path: {doc_str}"
     # WHEN / THEN
-    assert inspect_getdoc(create_fisc_owner_time_csv_path).replace("/", "\\") == doc_str
+    assert LINUX_OS or inspect_getdoc(create_fisc_owner_time_csv_path) == doc_str
 
 
 def test_create_fisc_owner_time_json_path_HasDocString():
@@ -39,9 +42,7 @@ def test_create_fisc_owner_time_json_path_HasDocString():
     doc_str = create_fisc_owner_time_json_path("fisc_mstr", "fisc_title")
     doc_str = f"Returns path: {doc_str}"
     # WHEN / THEN
-    assert (
-        inspect_getdoc(create_fisc_owner_time_json_path).replace("/", "\\") == doc_str
-    )
+    assert LINUX_OS or inspect_getdoc(create_fisc_owner_time_json_path) == doc_str
 
 
 def test_fisc_agenda_list_report_path_HasDocString():
@@ -49,7 +50,7 @@ def test_fisc_agenda_list_report_path_HasDocString():
     doc_str = fisc_agenda_list_report_path("fisc_mstr", "fisc_title")
     doc_str = f"Returns path: {doc_str}"
     # WHEN / THEN
-    assert inspect_getdoc(fisc_agenda_list_report_path).replace("/", "\\") == doc_str
+    assert LINUX_OS or inspect_getdoc(fisc_agenda_list_report_path) == doc_str
 
 
 def test_create_owners_dir_path_HasDocString():
@@ -57,7 +58,7 @@ def test_create_owners_dir_path_HasDocString():
     doc_str = create_owners_dir_path("fisc_mstr", "fisc_title")
     doc_str = f"Returns path: {doc_str}"
     # WHEN / THEN
-    assert inspect_getdoc(create_owners_dir_path).replace("/", "\\") == doc_str
+    assert LINUX_OS or inspect_getdoc(create_owners_dir_path) == doc_str
 
 
 def test_create_episodes_dir_path_HasDocString():
@@ -65,7 +66,7 @@ def test_create_episodes_dir_path_HasDocString():
     doc_str = create_episodes_dir_path("fisc_mstr", "fisc_title", "owner_name")
     doc_str = f"Returns path: {doc_str}"
     # WHEN / THEN
-    assert inspect_getdoc(create_episodes_dir_path).replace("/", "\\") == doc_str
+    assert LINUX_OS or inspect_getdoc(create_episodes_dir_path) == doc_str
 
 
 def test_create_timepoint_dir_path_HasDocString():
@@ -75,7 +76,7 @@ def test_create_timepoint_dir_path_HasDocString():
     )
     doc_str = f"Returns path: {doc_str}"
     # WHEN / THEN
-    assert inspect_getdoc(create_timepoint_dir_path).replace("/", "\\") == doc_str
+    assert LINUX_OS or inspect_getdoc(create_timepoint_dir_path) == doc_str
 
 
 def test_create_deal_path_HasDocString():
@@ -83,7 +84,7 @@ def test_create_deal_path_HasDocString():
     doc_str = create_deal_path("fisc_mstr", "fisc_title", "owner_name", "timepoint_int")
     doc_str = f"Returns path: {doc_str}"
     # WHEN / THEN
-    assert inspect_getdoc(create_deal_path).replace("/", "\\") == doc_str
+    assert LINUX_OS or inspect_getdoc(create_deal_path) == doc_str
 
 
 def test_create_budpoint_json_path_HasDocString():
@@ -93,7 +94,7 @@ def test_create_budpoint_json_path_HasDocString():
     )
     doc_str = f"Returns path: {doc_str}"
     # WHEN / THEN
-    assert inspect_getdoc(create_budpoint_json_path).replace("/", "\\") == doc_str
+    assert LINUX_OS or inspect_getdoc(create_budpoint_json_path) == doc_str
 
 
 def test_create_owner_event_dir_path_HasDocString():
@@ -103,7 +104,7 @@ def test_create_owner_event_dir_path_HasDocString():
     )
     doc_str = f"Returns path: {doc_str}"
     # WHEN / THEN
-    assert inspect_getdoc(create_owner_event_dir_path).replace("/", "\\") == doc_str
+    assert LINUX_OS or inspect_getdoc(create_owner_event_dir_path) == doc_str
 
 
 def test_create_event_bud_path_HasDocString():
@@ -113,7 +114,7 @@ def test_create_event_bud_path_HasDocString():
     )
     doc_str = f"Returns path: {doc_str}"
     # WHEN / THEN
-    assert inspect_getdoc(create_event_bud_path).replace("/", "\\") == doc_str
+    assert LINUX_OS or inspect_getdoc(create_event_bud_path) == doc_str
 
 
 def test_create_event_all_gift_path_HasDocString():
@@ -123,7 +124,7 @@ def test_create_event_all_gift_path_HasDocString():
     )
     doc_str = f"Returns path: {doc_str}"
     # WHEN / THEN
-    assert inspect_getdoc(create_event_all_gift_path).replace("/", "\\") == doc_str
+    assert LINUX_OS or inspect_getdoc(create_event_all_gift_path) == doc_str
 
 
 def test_create_event_expressed_gift_path_HasDocString():
@@ -133,9 +134,7 @@ def test_create_event_expressed_gift_path_HasDocString():
     )
     doc_str = f"Returns path: {doc_str}"
     # WHEN / THEN
-    assert (
-        inspect_getdoc(create_event_expressed_gift_path).replace("/", "\\") == doc_str
-    )
+    assert LINUX_OS or inspect_getdoc(create_event_expressed_gift_path) == doc_str
 
 
 def test_create_voice_path_HasDocString():
@@ -145,8 +144,8 @@ def test_create_voice_path_HasDocString():
     # WHEN / THEN
     print(f"{doc_str=}")
     print(f"{inspect_getdoc(create_voice_path)=}")
-    print(inspect_getdoc(create_voice_path).replace("/", "\\"))
-    assert inspect_getdoc(create_voice_path).replace("/", "\\") == doc_str
+    print(inspect_getdoc(create_voice_path))
+    assert LINUX_OS or inspect_getdoc(create_voice_path) == doc_str
 
 
 def test_create_forecast_path_HasDocString():
@@ -154,4 +153,4 @@ def test_create_forecast_path_HasDocString():
     doc_str = create_forecast_path("fisc_mstr", "fisc_title", "owner_name")
     doc_str = f"Returns path: {doc_str}"
     # WHEN / THEN
-    assert inspect_getdoc(create_forecast_path).replace("/", "\\") == doc_str
+    assert LINUX_OS or inspect_getdoc(create_forecast_path) == doc_str
