8f45c4b367d81a07d148a67d3fa0466da9155137
jschalk <joachim.schalk@gmail.com>
2025-02-17 17:13:35 -0800

    #505 get create_all_deal_node_facts_files to work for nested deal nodes

--- src/f07_fisc/fisc_tool.py
+++ src/f07_fisc/fisc_tool.py
@@ -1,13 +1,16 @@
 from src.f00_instrument.file import create_path, save_json, get_level1_dirs, open_json
+from src.f01_road.road import TitleUnit
 from src.f05_listen.hub_path import (
     create_deal_node_json_path,
     create_deal_node_facts_path,
 )
 from src.f05_listen.hub_tool import get_budevent_facts
+from os import walk as os_walk
 from os.path import exists as os_path_exists
 
 
-def create_all_deal_node_factunits(fisc_mstr_dir: str, fisc_title: str):
+def create_all_deal_node_facts_files(fisc_mstr_dir: str, fisc_title: str):
+    deal_node_filename = "deal_node.json"
     fiscs_dir = create_path(fisc_mstr_dir, "fiscs")
     for fisc_title in get_level1_dirs(fiscs_dir):
         fisc_dir = create_path(fiscs_dir, fisc_title)
@@ -16,16 +19,18 @@ def create_all_deal_node_factunits(fisc_mstr_dir: str, fisc_title: str):
             owner_dir = create_path(owners_dir, owner_name)
             deals_dir = create_path(owner_dir, "deals")
             for time_int in get_level1_dirs(deals_dir):
-                deal_node_json_path = create_deal_node_json_path(
-                    fisc_mstr_dir, fisc_title, owner_name, time_int
-                )
-                if os_path_exists(deal_node_json_path):
-                    deal_node = open_json(deal_node_json_path)
-                    deal_event_int = deal_node.get("event_int")
-                    budevent_fact_dict = get_budevent_facts(
-                        fisc_mstr_dir, fisc_title, owner_name, deal_event_int
-                    )
-                    deal_node_facts_path = create_deal_node_facts_path(
-                        fisc_mstr_dir, fisc_title, owner_name, time_int
-                    )
-                    save_json(deal_node_facts_path, None, budevent_fact_dict)
+                deal_time_dir = create_path(deals_dir, time_int)
+                for dirpath, dirnames, filenames in os_walk(deal_time_dir):
+                    if deal_node_filename in set(filenames):
+                        deal_node_json_path = create_path(dirpath, deal_node_filename)
+                        deal_node_dict = open_json(deal_node_json_path)
+                        deal_event_int = deal_node_dict.get("event_int")
+                        budevent_fact_dict = get_budevent_facts(
+                            fisc_mstr_dir, fisc_title, owner_name, deal_event_int
+                        )
+                        deal_node_facts_path = create_path(dirpath, "facts.json")
+                        save_json(deal_node_facts_path, None, budevent_fact_dict)
+
+
+# def inherit_all_deal_node_face_files(fisc_mstr_dir: str, fiscal_title: TitleUnit):
+#     pass
--- src/f07_fisc/test_fisc_tool/test_deal_facts.py
+++ src/f07_fisc/test_fisc_tool/test_deal_facts.py
@@ -2,17 +2,21 @@ from src.f00_instrument.file import open_json
 from src.f01_road.road import create_road
 from src.f04_gift.atom_config import base_str
 from src.f05_listen.hub_path import (
+    create_deal_node_json_path as node_path,
     create_deal_node_facts_path as facts_path,
     create_budevent_path,
 )
 from src.f05_listen.hub_tool import save_arbitrary_budevent, save_arbitrary_dealnode
-from src.f07_fisc.fisc_tool import create_all_deal_node_factunits
+from src.f07_fisc.fisc_tool import (
+    create_all_deal_node_facts_files,
+    # inherit_all_deal_node_face_files,
+)
 from src.f07_fisc.examples.fisc_env import env_dir_setup_cleanup, get_test_fisc_mstr_dir
 from os.path import exists as os_path_exists
 
 
 # for ever deal node create all factunits
-def test_create_all_deal_node_factunits_SetsFiles_Scenario0_NoFacts(
+def test_create_all_deal_node_facts_files_SetsFiles_Scenario0_NoFacts(
     env_dir_setup_cleanup,
 ):
     # ESTABLISH
@@ -30,7 +34,7 @@ def test_create_all_deal_node_factunits_SetsFiles_Scenario0_NoFacts(
     assert os_path_exists(bob_t5_facts_path) is False
 
     # WHEN
-    create_all_deal_node_factunits(fisc_mstr_dir, a23_str)
+    create_all_deal_node_facts_files(fisc_mstr_dir, a23_str)
 
     # THEN
     assert os_path_exists(bob_t5_facts_path)
@@ -38,7 +42,7 @@ def test_create_all_deal_node_factunits_SetsFiles_Scenario0_NoFacts(
 
 
 # for ever deal node create all factunits
-def test_create_all_deal_node_factunits_SetsFiles_Scenario1_WithFacts(
+def test_create_all_deal_node_facts_files_SetsFiles_Scenario1_WithFacts(
     env_dir_setup_cleanup,
 ):
     # ESTABLISH
@@ -59,10 +63,45 @@ def test_create_all_deal_node_factunits_SetsFiles_Scenario1_WithFacts(
     assert os_path_exists(bob_t5_facts_path) is False
 
     # WHEN
-    create_all_deal_node_factunits(fisc_mstr_dir, a23_str)
+    create_all_deal_node_facts_files(fisc_mstr_dir, a23_str)
 
     # THEN
     assert os_path_exists(bob_t5_facts_path)
     assert open_json(bob_t5_facts_path) == {
         casa_road: {base_str(): casa_road, "pick": clean_road}
     }
+
+
+# for ever deal node create all factunits
+def test_create_all_deal_node_facts_files_SetsFiles_Scenario2_WithFacts_NotAtRoot(
+    env_dir_setup_cleanup,
+):
+    # ESTABLISH
+    fisc_mstr_dir = get_test_fisc_mstr_dir()
+    bob_str = "Bob"
+    a23_str = "accord23"
+    event300 = 300
+    time5 = 5
+    casa_road = create_road(a23_str, "casa")
+    clean_road = create_road(casa_road, "clean")
+    x_facts = [(casa_road, clean_road, None, None)]
+    save_arbitrary_budevent(fisc_mstr_dir, a23_str, bob_str, event300, facts=x_facts)
+    bob3_budevent_path = create_budevent_path(fisc_mstr_dir, a23_str, bob_str, event300)
+    yao_str = "Yao"
+    das = [yao_str, bob_str]
+    bob_5_yao_bob_deal_node = node_path(fisc_mstr_dir, a23_str, bob_str, time5, das)
+    assert os_path_exists(bob_5_yao_bob_deal_node) is False
+    save_arbitrary_dealnode(fisc_mstr_dir, a23_str, bob_str, time5, das, event300)
+    print(f"{bob_5_yao_bob_deal_node=}")
+    assert os_path_exists(bob_5_yao_bob_deal_node)
+    bob_t5_facts_path = facts_path(fisc_mstr_dir, a23_str, bob_str, time5, das)
+    print(f" {bob_t5_facts_path=}")
+    assert os_path_exists(bob_t5_facts_path) is False
+
+    # WHEN
+    create_all_deal_node_facts_files(fisc_mstr_dir, a23_str)
+
+    # THEN
+    assert os_path_exists(bob_t5_facts_path)
+    facts_dict = open_json(bob_t5_facts_path)
+    assert facts_dict == {casa_road: {base_str(): casa_road, "pick": clean_road}}
