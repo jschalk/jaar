dca20634269d236f3b9e9e861d1b25481700148b
jschalk <joachim.schalk@gmail.com>
2025-02-26 10:50:30 -0800

    #505 added budevent facts to CellUnit

--- src/f05_listen/cell.py
+++ src/f05_listen/cell.py
@@ -1,16 +1,22 @@
 from src.f00_instrument.dict_toolbox import (
     get_empty_list_if_None,
+    get_empty_dict_if_None,
     get_0_if_None,
     get_1_if_None,
 )
 from src.f01_road.finance import PennyNum
 from src.f01_road.road import OwnerName, EventInt, RoadUnit
-from src.f02_bud.reason_item import FactUnit
+from src.f02_bud.reason_item import (
+    FactUnit,
+    factunits_get_from_dict,
+    get_dict_from_factunits,
+)
 from src.f02_bud.bud import (
     BudUnit,
     budunit_shop,
     get_from_dict as budunit_get_from_dict,
 )
+from src.f02_bud.bud_tool import get_bud_root_facts_dict
 from dataclasses import dataclass
 
 CELL_NODE_QUOTA_DEFAULT = 1000
@@ -25,6 +31,7 @@ class CellUnit:
     penny: PennyNum = None
     quota: float = None
     budadjust: BudUnit = None
+    budevent_facts: dict[RoadUnit, FactUnit] = None
     found_facts: dict[RoadUnit, FactUnit] = None
     boss_facts: dict[RoadUnit, FactUnit] = None
 
@@ -37,8 +44,9 @@ class CellUnit:
             "penny": self.penny,
             "quota": self.quota,
             "budadjust": self.budadjust,
-            "found_facts": self.found_facts,
-            "boss_facts": self.boss_facts,
+            "budevent_facts": get_dict_from_factunits(self.budevent_facts),
+            "found_facts": get_dict_from_factunits(self.found_facts),
+            "boss_facts": get_dict_from_factunits(self.boss_facts),
         }
 
 
@@ -50,6 +58,7 @@ def cellunit_shop(
     penny: PennyNum = None,
     quota: float = None,
     budadjust: BudUnit = None,
+    budevent_facts: dict[RoadUnit, FactUnit] = None,
     found_facts: dict[RoadUnit, FactUnit] = None,
     boss_facts: dict[RoadUnit, FactUnit] = None,
 ) -> CellUnit:
@@ -62,4 +71,8 @@ def cellunit_shop(
         deal_owner_name=deal_owner_name,
         penny=get_1_if_None(penny),
         quota=quota,
+        budadjust=budadjust,
+        budevent_facts=get_empty_dict_if_None(budevent_facts),
+        found_facts=get_empty_dict_if_None(found_facts),
+        boss_facts=get_empty_dict_if_None(boss_facts),
     )
--- src/f05_listen/test_cell/test_cell_.py
+++ src/f05_listen/test_cell/test_cell_.py
@@ -1,4 +1,10 @@
+from src.f02_bud.bud import budunit_shop
 from src.f05_listen.cell import CellUnit, cellunit_shop, CELL_NODE_QUOTA_DEFAULT
+from src.f05_listen.examples.example_listen import (
+    example_casa_clean_factunit as clean_factunit,
+    example_casa_dirty_factunit as dirty_factunit,
+    example_sky_blue_factunit as sky_blue_factunit,
+)
 
 
 def test_CELL_NODE_QUOTA_DEFAULT_value():
@@ -17,6 +23,7 @@ def test_CellUnit_Exists():
     assert not x_cellunit.penny
     assert not x_cellunit.quota
     assert not x_cellunit.budadjust
+    assert not x_cellunit.budevent_facts
     assert not x_cellunit.found_facts
     assert not x_cellunit.boss_facts
 
@@ -32,8 +39,9 @@ def test_cellunit_shop_ReturnsObj_Scenario0_WithoutParameters():
     assert x_cellunit.penny == 1
     assert x_cellunit.quota == CELL_NODE_QUOTA_DEFAULT
     assert not x_cellunit.budadjust
-    assert x_cellunit.found_facts is None
-    assert x_cellunit.boss_facts is None
+    assert x_cellunit.budevent_facts == {}
+    assert x_cellunit.found_facts == {}
+    assert x_cellunit.boss_facts == {}
 
 
 def test_cellunit_shop_ReturnsObj_Scenario1_WithParameters():
@@ -47,6 +55,14 @@ def test_cellunit_shop_ReturnsObj_Scenario1_WithParameters():
     bob_sue_celldepth3 = 3
     bob_sue_penny2 = 2
     bob_sue_quota300 = 300
+    bob_sue_bud = budunit_shop(sue_str)
+    bob_sue_bud.add_acctunit(bob_str, 7, 13)
+    clean_fact = clean_factunit()
+    dirty_fact = dirty_factunit()
+    sky_blue_fact = sky_blue_factunit()
+    bob_sue_budevent_factunits = {clean_fact.base: clean_fact}
+    bob_sue_found_factunits = {dirty_fact.base: dirty_fact}
+    bob_sue_boss_factunits = {sky_blue_fact.base: sky_blue_fact}
 
     # WHEN
     x_cellunit = cellunit_shop(
@@ -56,6 +72,10 @@ def test_cellunit_shop_ReturnsObj_Scenario1_WithParameters():
         bob_sue_deal_owner,
         bob_sue_penny2,
         bob_sue_quota300,
+        bob_sue_bud,
+        bob_sue_budevent_factunits,
+        bob_sue_found_factunits,
+        bob_sue_boss_factunits,
     )
 
     # THEN
@@ -65,9 +85,10 @@ def test_cellunit_shop_ReturnsObj_Scenario1_WithParameters():
     assert x_cellunit.deal_owner_name == bob_sue_deal_owner
     assert x_cellunit.penny == bob_sue_penny2
     assert x_cellunit.quota == bob_sue_quota300
-    assert x_cellunit.budadjust is None
-    assert x_cellunit.found_facts is None
-    assert x_cellunit.boss_facts is None
+    assert x_cellunit.budadjust == bob_sue_bud
+    assert x_cellunit.budevent_facts == bob_sue_budevent_factunits
+    assert x_cellunit.found_facts == bob_sue_found_factunits
+    assert x_cellunit.boss_facts == bob_sue_boss_factunits
 
 
 def test_CellUnit_get_dict_ReturnsObj_Scenario0():
@@ -102,6 +123,7 @@ def test_CellUnit_get_dict_ReturnsObj_Scenario0():
         "penny",
         "quota",
         "budadjust",
+        "budevent_facts",
         "found_facts",
         "boss_facts",
     ]
@@ -112,5 +134,67 @@ def test_CellUnit_get_dict_ReturnsObj_Scenario0():
     assert x_cell_dict.get("penny") == bob_sue_penny2
     assert x_cell_dict.get("quota") == bob_sue_quota300
     assert x_cell_dict.get("budadjust") is None
-    assert x_cell_dict.get("found_facts") is None
-    assert x_cell_dict.get("boss_facts") is None
+    assert x_cell_dict.get("budevent_facts") == {}
+    assert x_cell_dict.get("found_facts") == {}
+    assert x_cell_dict.get("boss_facts") == {}
+
+
+def test_CellUnit_get_dict_ReturnsObj_Scenario1_WithMoreParameters():
+    # ESTABLISH
+    yao_str = "Yao"
+    bob_str = "Bob"
+    sue_str = "Sue"
+    bob_sue_ancestors = [bob_str, sue_str]
+    bob_sue_event7 = 7
+    bob_sue_deal_owner = yao_str
+    bob_sue_celldepth3 = 3
+    bob_sue_penny2 = 2
+    bob_sue_quota300 = 300
+    clean_fact = clean_factunit()
+    dirty_fact = dirty_factunit()
+    sky_blue_fact = sky_blue_factunit()
+    bob_sue_budevent_factunits = {clean_fact.base: clean_fact}
+    bob_sue_found_factunits = {dirty_fact.base: dirty_fact}
+    bob_sue_boss_factunits = {sky_blue_fact.base: sky_blue_fact}
+    x_cellunit = cellunit_shop(
+        bob_sue_ancestors,
+        bob_sue_event7,
+        bob_sue_celldepth3,
+        bob_sue_deal_owner,
+        bob_sue_penny2,
+        bob_sue_quota300,
+        None,
+        bob_sue_budevent_factunits,
+        bob_sue_found_factunits,
+        bob_sue_boss_factunits,
+    )
+
+    # WHEN
+    x_cell_dict = x_cellunit.get_dict()
+
+    # THEN
+    assert list(x_cell_dict.keys()) == [
+        "ancestors",
+        "event_int",
+        "celldepth",
+        "deal_owner_name",
+        "penny",
+        "quota",
+        "budadjust",
+        "budevent_facts",
+        "found_facts",
+        "boss_facts",
+    ]
+    assert x_cell_dict.get("ancestors") == bob_sue_ancestors
+    assert x_cell_dict.get("event_int") == bob_sue_event7
+    assert x_cell_dict.get("celldepth") == bob_sue_celldepth3
+    assert x_cell_dict.get("deal_owner_name") == bob_sue_deal_owner
+    assert x_cell_dict.get("penny") == bob_sue_penny2
+    assert x_cell_dict.get("quota") == bob_sue_quota300
+    assert x_cell_dict.get("budadjust") is None
+    bob_sue_budevent_fact_dicts = {clean_fact.base: clean_fact.get_dict()}
+    bob_sue_found_fact_dicts = {dirty_fact.base: dirty_fact.get_dict()}
+    bob_sue_boss_fact_dicts = {sky_blue_fact.base: sky_blue_fact.get_dict()}
+    assert x_cell_dict.get("budevent_facts") == bob_sue_budevent_fact_dicts
+    assert x_cell_dict.get("found_facts") == bob_sue_found_fact_dicts
+    assert x_cell_dict.get("boss_facts") == bob_sue_boss_fact_dicts
