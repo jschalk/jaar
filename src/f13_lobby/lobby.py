dca8525d2c503b9ddb746bfb81a5707eb3d39df6
jschalk <joachim.schalk@gmail.com>
2025-04-14 10:00:27 -0700

    #550 create lobbyunit_shop

---
+++ src/f13_lobby/lobby.py
@@ -0,0 +1,20 @@
+from src.f04_pack.pack import PackUnit
+from src.f12_world.world import WorldUnit
+from dataclasses import dataclass
+
+
+@dataclass
+class LobbyUnit:
+    lobby_id: str = None
+    option_packs: list[PackUnit] = None
+    selected_pack: PackUnit = None
+    worlds: list[WorldUnit] = None
+
+
+def lobbyunit_shop(
+    lobby_id: str,
+    option_packs: list[PackUnit] = None,
+    selected_pack: PackUnit = None,
+    worlds: list[WorldUnit] = None,
+) -> LobbyUnit:
+    return LobbyUnit(lobby_id=lobby_id)
