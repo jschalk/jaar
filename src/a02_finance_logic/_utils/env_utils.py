7da2e57297bb68945e3bfa2aa5ba07e3f114f7eb
jschalk <joachim.schalk@gmail.com>
2025-04-28 22:41:04 -0700

    cleanup

--- src/a02_finance_logic/_utils/env_utils.py
+++ src/a02_finance_logic/_utils/env_utils.py
@@ -3,7 +3,7 @@ from pytest import fixture as pytest_fixture
 
 
 def get_module_temp_dir():
-    return "src/a02_finance_toolboxs/_utils/temp"
+    return "src/a02_finance_logic/_utils/temp"
 
 
 @pytest_fixture()
