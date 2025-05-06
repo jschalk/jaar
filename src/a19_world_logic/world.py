2856a6468f4ad021ebe5f8d83acc94625a3019e1
jschalk <joachim.schalk@gmail.com>
2025-05-06 14:26:06 -0700

    closes #737

--- src/a19_world_logic/world.py
+++ src/a19_world_logic/world.py
@@ -20,7 +20,7 @@ from src.a18_etl_toolbox.transformers import (
     etl_brick_agg_tables_to_brick_valid_tables,
     etl_brick_valid_tables_to_sound_raw_tables,
     etl_sound_raw_tables_to_sound_agg_tables,
-    insert_pidgin_sound_agg_into_pidgin_core_raw_tables,
+    etl_sound_agg_to_pidgin_core_tables,
     etl_brick_raw_db_to_brick_raw_df,
     etl_brick_agg_tables_to_brick_agg_dfs,
     etl_brick_raw_tables_to_events_brick_agg_table,
@@ -329,7 +329,7 @@ class WorldUnit:
         etl_brick_agg_tables_to_brick_valid_tables(cursor)
         etl_brick_valid_tables_to_sound_raw_tables(cursor)
         etl_sound_raw_tables_to_sound_agg_tables(cursor)
-        insert_pidgin_sound_agg_into_pidgin_core_raw_tables(cursor)
+        etl_sound_agg_to_pidgin_core_tables(cursor)
 
         # identify all idea data that has conflicting face_name/event_int uniqueness
         # self._events = etl_events_brick_agg_db_to_event_dict(cursor)
