1b57455cca3bd140ba9097daaede0c1ab601c299
jschalk <joachim.schalk@gmail.com>
2025-06-16 22:48:42 -0700

    closes #806, closes #807 fixed import bug

--- src/a18_etl_toolbox/test_tran/test_brick_raw_tables_to_brick_agg_tables.py
+++ src/a18_etl_toolbox/test_tran/test_brick_raw_tables_to_brick_agg_tables.py
@@ -7,8 +7,9 @@ from src.a00_data_toolbox.db_toolbox import (
 from src.a02_finance_logic._util.a02_str import vow_label_str
 from src.a09_pack_logic._util.a09_str import event_int_str, face_name_str
 from src.a15_vow_logic._util.a15_str import cumlative_minute_str, hour_label_str
-from src.a17_idea_logic._util.a17_str import brick_agg_str, brick_raw_str
+from src.a17_idea_logic._util.a17_str import brick_raw_str
 from src.a17_idea_logic.idea_db_tool import create_idea_sorted_table
+from src.a18_etl_toolbox._util.a18_str import brick_agg_str
 from src.a18_etl_toolbox.transformers import etl_brick_raw_tables_to_brick_agg_tables
 
 
