6de86a2de3a484e2a5d2823ae9b62a6395fa3a75
Joachim <jschalk@users.noreply.github.com>
2025-09-29 10:43:25 -0600

    #984 confirmed all ch03 str functions removed

--- src/ch03_finance_logic/_ref/ch03_keywords.py
+++ src/ch03_finance_logic/_ref/ch03_keywords.py
@@ -1,50 +1,6 @@
 from src.ch02_rope_logic._ref.ch02_keywords import *
 
 
-def BitNum_str() -> str:
-    return "BitNum"
-
-
-def FundIota_str() -> str:
-    return "FundIota"
-
-
-def FundNum_str() -> str:
-    return "FundNum"
-
-
-def GrainFloat_str() -> str:
-    return "GrainFloat"
-
-
-def MoneyUnit_str() -> str:
-    return "MoneyUnit"
-
-
-def PennyNum_str() -> str:
-    return "PennyNum"
-
-
-def RespectNum_str() -> str:
-    return "RespectNum"
-
-
-def fund_iota_str() -> str:
-    return "fund_iota"
-
-
-def fund_pool_str() -> str:
-    return "fund_pool"
-
-
-def magnitude_str() -> str:
-    return "magnitude"
-
-
-def penny_str() -> str:
-    return "penny"
-
-
 class Ch03Keywords(str, Enum):
     BitNum = "BitNum"
     FundIota = "FundIota"
--- src/ch03_finance_logic/test/_util/test_ch03_keywords.py
+++ src/ch03_finance_logic/test/_util/test_ch03_keywords.py
@@ -1,17 +1,4 @@
-from src.ch03_finance_logic._ref.ch03_keywords import (
-    BitNum_str,
-    Ch03Keywords,
-    FundIota_str,
-    FundNum_str,
-    GrainFloat_str,
-    MoneyUnit_str,
-    PennyNum_str,
-    RespectNum_str,
-    fund_iota_str,
-    fund_pool_str,
-    magnitude_str,
-    penny_str,
-)
+from src.ch03_finance_logic._ref.ch03_keywords import Ch03Keywords
 
 
 def test_Ch03Keywords_AttributeNamesEqualValues():
@@ -27,14 +14,4 @@ def test_Ch03Keywords_AttributeNamesEqualValues():
 
 def test_str_functions_ReturnsObj():
     # ESTABLISH / WHEN / THEN
-    assert BitNum_str() == "BitNum"
-    assert FundIota_str() == "FundIota"
-    assert GrainFloat_str() == "GrainFloat"
-    assert FundNum_str() == "FundNum"
-    assert MoneyUnit_str() == "MoneyUnit"
-    assert PennyNum_str() == "PennyNum"
-    assert RespectNum_str() == "RespectNum"
-    assert fund_pool_str() == "fund_pool"
-    assert fund_iota_str() == "fund_iota"
-    assert magnitude_str() == "magnitude"
-    assert penny_str() == "penny"
+    pass
