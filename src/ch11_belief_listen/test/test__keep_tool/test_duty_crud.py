906cd70361485ff92f0618e7fc7797eb350305c4
Joachim <jschalk@users.noreply.github.com>
2025-10-24 09:21:52 -0600

    #1014 cleanup

--- src/ch11_belief_listen/test/test__keep_tool/test_duty_crud.py
+++ src/ch11_belief_listen/test/test__keep_tool/test_duty_crud.py
@@ -1,6 +1,5 @@
 from src.ch04_rope.rope import create_rope, default_knot_if_None
 from src.ch07_belief_logic.test._util.ch07_examples import get_beliefunit_with_4_levels
-from src.ch10_bud.test._util.ch10_examples import get_ch10_example_moment_label
 from src.ch11_belief_listen.keep_tool import (
     get_vision_belief,
     save_vision_belief,
@@ -10,13 +9,14 @@ from src.ch11_belief_listen.test._util.ch11_env import (
     get_temp_dir as env_dir,
     temp_dir_setup,
 )
+from src.ch11_belief_listen.test._util.ch11_examples import ch11_example_moment_label
 
 
 def test_save_vision_belief_SavesFile(temp_dir_setup):
     # ESTABLISH
     sue_str = "Sue"
     nation_str = "nation"
-    nation_rope = create_rope(get_ch10_example_moment_label(), nation_str)
+    nation_rope = create_rope(ch11_example_moment_label(), nation_str)
     usa_str = "USA"
     usa_rope = create_rope(nation_rope, usa_str)
     texas_str = "Texas"
@@ -42,7 +42,7 @@ def test_vision_file_exists_ReturnsBool(temp_dir_setup):
     # ESTABLISH
     sue_str = "Sue"
     nation_str = "nation"
-    nation_rope = create_rope(get_ch10_example_moment_label(), nation_str)
+    nation_rope = create_rope(ch11_example_moment_label(), nation_str)
     usa_str = "USA"
     usa_rope = create_rope(nation_rope, usa_str)
     texas_str = "Texas"
@@ -68,7 +68,7 @@ def test_get_vision_belief_reason_lowersFile(temp_dir_setup):
     # ESTABLISH
     sue_str = "Sue"
     nation_str = "nation"
-    nation_rope = create_rope(get_ch10_example_moment_label(), nation_str)
+    nation_rope = create_rope(ch11_example_moment_label(), nation_str)
     usa_str = "USA"
     usa_rope = create_rope(nation_rope, usa_str)
     texas_str = "Texas"
@@ -96,7 +96,7 @@ def test_get_vision_belief_ReturnsNoneIfFileDoesNotExist(
     # ESTABLISH
     sue_str = "Sue"
     nation_str = "nation"
-    nation_rope = create_rope(get_ch10_example_moment_label(), nation_str)
+    nation_rope = create_rope(ch11_example_moment_label(), nation_str)
     usa_str = "USA"
     usa_rope = create_rope(nation_rope, usa_str)
     texas_str = "Texas"
